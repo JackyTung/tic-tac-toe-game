117b97e1ab82a0619fff5ecafd96bc300d4947b7
Jacky Tung <jacky.tung@langlive.com>
2022-04-05 11:39:40 +0800

    feat: init checkbox style

--- src/components/CustomCheckBox/styles.js
+++ src/components/CustomCheckBox/styles.js
@@ -7,4 +7,16 @@ export const StyledContainer = styled.div`
   box-shadow: 7px 7px 5px rgba(0, 0, 0, 0.04),
     -7px -7px 5px rgba(255, 255, 255, 0.8);
   border: 2px solid rgb(235, 235, 235);
+
+  input[type="checkbox"] {
+    display: none;
+    pointer-events: none;
+
+    & + label {
+      cursor: pointer;
+      font-size: 0.8em;
+      display: grid;
+      grid-template-columns: auto 3fr;
+    }
+  }
 `;
--- src/containers/SvgPlayGround/index.jsx
+++ src/containers/SvgPlayGround/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 import CustomCheckBox from "@/components/CustomCheckBox/index.jsx";
-
+import { StyledContent, StyledTitle } from "./styles";
 const SvgPlayGround = () => {
   return (
     <div>
-      This is SvgPlayGround
-      <CustomCheckBox />
+      <StyledTitle>This is SvgPlayGround</StyledTitle>
+      <StyledContent>
+        <CustomCheckBox />
+      </StyledContent>
     </div>
   );
 };
