4ef161b26ba1b1fbae4fddecd0dde59a451ea142
Jacky Tung <jacky.tung@langlive.com>
2022-04-05 21:24:34 +0800

    feat: add canvas playground page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import React from "react";
 import { Provider } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 
+import CanvasPlayGround from "@/containers/CanvasPlayGround/index.jsx";
 import Game from "@/containers/Game/index.jsx";
 import Home from "@/containers/Home/index.jsx";
 import SvgPlayGround from "@/containers/SvgPlayGround/index.jsx";
@@ -19,6 +20,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="game" element={<Game />} />
           <Route path="svg-playground" element={<SvgPlayGround />} />
+          <Route path="canvas-playground" element={<CanvasPlayGround />} />
         </Routes>
       </Provider>
     </div>
---
+++ src/containers/CanvasPlayGround/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { StyledContent, StyledTitle } from "./styles";
+
+const CanvasPlayGround = () => {
+  return (
+    <div>
+      <StyledTitle>Canvas play ground</StyledTitle>
+      <StyledContent>content</StyledContent>
+    </div>
+  );
+};
+
+export default CanvasPlayGround;
